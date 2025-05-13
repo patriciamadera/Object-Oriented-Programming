b4b351720e6373ed008e790ed98b4024d6be2297
patriciamadera <patriciamadera92@gmail.com>
2025-05-13 19:58:13 -0400

    fix

--- Ecommerce_Project/src/Model/RegularClient.java
+++ Ecommerce_Project/src/Model/RegularClient.java
@@ -1,11 +1,28 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RegularClient extends Client {
+	private List<String> purchaseHistory;
 
 	public RegularClient(int id, String name, String lastName, String email, String password, String address,
 			String phoneNumber, String gender) {
 		super(id, name, lastName, email, password, address, phoneNumber, gender);
-		// TODO Auto-generated constructor stub
+		this.purchaseHistory = new ArrayList<>();
 	}
+	
+	public void addPurchase(String productId) {
+        purchaseHistory.add(productId);
+    }
+
+    public List<String> getPurchaseHistory() {
+        return purchaseHistory;
+    }
+
+    @Override
+    public void displayRole() {
+        System.out.println("Cliente regular: " + name);
+    }
 
 }
